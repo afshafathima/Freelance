d73cca52e1ae6125bb070014be4940f90bd27610
afshafathima <fafsha349@gmail.com>
2026-07-22 20:57:13 +0530

    Fix React Router for Vercel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
-﻿import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+﻿import { Routes, Route, Navigate } from "react-router-dom";
+
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
@@ -11,47 +12,100 @@ import Financials from "./pages/Financials";
 import Layout from "./components/Layout";
 
 const PrivateRoute = ({ children }) => {
-    const token = localStorage.getItem("token");
-    return token ? children : <Navigate to="/" replace />;
+  const token = localStorage.getItem("token");
+  return token ? children : <Navigate to="/" replace />;
 };
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route
-                    path="/dashboard"
-                    element={<PrivateRoute><Layout><Dashboard /></Layout></PrivateRoute>}
-                />
-                <Route
-                    path="/clients"
-                    element={<PrivateRoute><Layout><Client /></Layout></PrivateRoute>}
-                />
-                <Route
-                    path="/projects"
-                    element={<PrivateRoute><Layout><Projects /></Layout></PrivateRoute>}
-                />
-                <Route
-                    path="/tasks"
-                    element={<PrivateRoute><Layout><Tasks /></Layout></PrivateRoute>}
-                />
-                <Route
-                    path="/timer"
-                    element={<PrivateRoute><Layout><TimeTracking /></Layout></PrivateRoute>}
-                />
-                <Route
-                    path="/invoices"
-                    element={<PrivateRoute><Layout><Invoices /></Layout></PrivateRoute>}
-                />
-                <Route
-                    path="/financials"
-                    element={<PrivateRoute><Layout><Financials /></Layout></PrivateRoute>}
-                />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <Routes>
+      {/* Public Routes */}
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+
+      {/* Protected Routes */}
+      <Route
+        path="/dashboard"
+        element={
+          <PrivateRoute>
+            <Layout>
+              <Dashboard />
+            </Layout>
+          </PrivateRoute>
+        }
+      />
+
+      <Route
+        path="/clients"
+        element={
+          <PrivateRoute>
+            <Layout>
+              <Client />
+            </Layout>
+          </PrivateRoute>
+        }
+      />
+
+      <Route
+        path="/projects"
+        element={
+          <PrivateRoute>
+            <Layout>
+              <Projects />
+            </Layout>
+          </PrivateRoute>
+        }
+      />
+
+      <Route
+        path="/tasks"
+        element={
+          <PrivateRoute>
+            <Layout>
+              <Tasks />
+            </Layout>
+          </PrivateRoute>
+        }
+      />
+
+      <Route
+        path="/timer"
+        element={
+          <PrivateRoute>
+            <Layout>
+              <TimeTracking />
+            </Layout>
+          </PrivateRoute>
+        }
+      />
+
+      <Route
+        path="/invoices"
+        element={
+          <PrivateRoute>
+            <Layout>
+              <Invoices />
+            </Layout>
+          </PrivateRoute>
+        }
+      />
+
+      <Route
+        path="/financials"
+        element={
+          <PrivateRoute>
+            <Layout>
+              <Financials />
+            </Layout>
+          </PrivateRoute>
+        }
+      />
+
+      {/* Redirect unknown routes */}
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
 }
 
 export default App;
